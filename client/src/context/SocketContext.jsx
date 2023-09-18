d84595853e77ae92300187a33729556d79a47100
Nishant <nishantjain2503@gmail.com>
2023-09-18 17:56:32 +0530

    changed url for websocket

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -1,5 +1,5 @@
 import { createContext } from "react"
 import socketio from "socket.io-client"
 
-export const socket = socketio.connect("http://localhost:3005")
+export const socket = socketio.connect("https://collabinator-server.onrender.com")
 export const SocketContext = createContext()
